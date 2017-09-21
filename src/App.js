f8aa91de3d4ff833b5243c0dc41b47197d469fc8
Rafael Pereira <rpstos@gmail.com>
2017-09-21 15:51:00 -0300

    Humany :: Apresentação

--- src/App.js
+++ src/App.js
@@ -5,16 +5,22 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
+        {/* <div className="App-header">
           <div className="App-logo">
-            <h2><span>_h</span>umany</h2>
+            <h2><span className="paint-word">_h</span>umany</h2>
             <small>Web & Apps development.</small>
           </div>
+        </div> */}
+        <div className="App-intro">
+          <h1>Rafael Pereira dos Santos</h1>
+          <ul>
+            <li>Sênior developer - <a target="_blank" href="http://pixeon.com.br">Pixeon</a> - São Paulo / SP</li> 
+            <li><a target="_blank" href="https://twitter.com/rafaelpstos">Twitter</a></li>
+            <li><a target="_blank" href="https://www.instagram.com/rafael82/">Instagram</a></li>
+            <li><a target="_blank" href="https://github.com/rafaelpstos">Github</a></li>
+            <li><a target="_blank" href="https://www.linkedin.com/in/rafaelpstos/">Linkedin</a></li>
+          </ul>         
         </div>
-        <p className="App-intro">
-          Em breve <br/>
-          rpstos@gmail.com
-        </p>
       </div>
     );
   }
